0b4fe146d44e687eac9ab8c96fe3ae4dcded2b70
Madeline <Madeline@DESKTOP-6PHQKKI>
2020-12-15 17:46:23 -0300

    comit inicial con eclipse. Close #1

---
+++ proyecto-PaginaWeb/src/test/java/com/madev/springboot/app/ProyectoPaginaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.madev.springboot.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPaginaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
